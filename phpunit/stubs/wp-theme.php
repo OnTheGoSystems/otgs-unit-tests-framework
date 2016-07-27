dad2a7d3d433d2ba6f9f8561fe009d674c71e112
Andrea Sciamanna <andrea.s@icanlocalize.com>
2016-07-27 11:35:56 +0200

    Added `WP_Theme` class stub

---
+++ phpunit/stubs/wp-theme.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * WP_Theme Class
+ * @package    WordPress
+ * @subpackage Theme
+ * @since      3.4.0
+ */
+class WP_Theme {
+	public function get( $something ) {
+		return $something;
+	}
+}
