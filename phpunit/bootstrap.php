b5dc58423daf747315bae569f7463e6b9d350f7f
Andrea Sciamanna <andrea.s@icanlocalize.com>
2016-08-03 12:21:36 +0200

    Addded some constants used in the other tests, and which might be needed for future tests.

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -9,3 +9,10 @@ require_once __DIR__ . '/utils/functions.php';
 
 define( 'OTGS_TESTS_MAIN_FILE', __DIR__ . '/../../plugin.php' );
 define( 'OTGS_PATH', dirname( OTGS_TESTS_MAIN_FILE ) );
+
+define( 'MINUTE_IN_SECONDS', 60 );
+define( 'HOUR_IN_SECONDS',   60 * MINUTE_IN_SECONDS );
+define( 'DAY_IN_SECONDS',    24 * HOUR_IN_SECONDS   );
+define( 'WEEK_IN_SECONDS',    7 * DAY_IN_SECONDS    );
+define( 'MONTH_IN_SECONDS',  30 * DAY_IN_SECONDS    );
+define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
