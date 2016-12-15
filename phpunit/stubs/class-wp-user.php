f340e025c47acebf0820e989da2ea21065a470f3
Jakub Bis <jakub.b@icanlocalize.com>
2016-12-15 18:36:36 +0800

    Add property to WP_User stub

--- phpunit/stubs/class-wp-user.php
+++ phpunit/stubs/class-wp-user.php
@@ -6,11 +6,15 @@
 class WP_User {
 	public $ID = 0;
 
+	public $user_email;
+
 	public function __construct( $id = 0, $name = '' ) {
 		$this->ID           = $id;
 		$this->display_name = $name;
 		$this->nickname     = $name;
 		$this->first_name   = 'First ' . $name;
 		$this->last_name    = 'Last ' . $name;
+
+		$this->user_email = 'translator' . $id . '@test.com';
 	}
 }
