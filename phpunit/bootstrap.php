d55835f3efedde08accaa5ce2ef8e7d84d7af12a
Andrea Sciamanna <andrea.s@onthegosystems.com>
2017-07-28 14:00:00 +0200

    Fix dependencies issues caused by `otgs/unit-tests-framework`

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -16,3 +16,5 @@ define( 'DAY_IN_SECONDS',    24 * HOUR_IN_SECONDS   );
 define( 'WEEK_IN_SECONDS',    7 * DAY_IN_SECONDS    );
 define( 'MONTH_IN_SECONDS',  30 * DAY_IN_SECONDS    );
 define( 'YEAR_IN_SECONDS',  365 * DAY_IN_SECONDS    );
+
+\tad\FunctionMocker\FunctionMocker::init();
