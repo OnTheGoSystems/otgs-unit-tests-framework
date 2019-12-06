3cdf07ae8fd20147366cadcb0e361f030827f1f7
Igor Gergel <igor.g@onthegosystems.com>
2019-12-06 17:35:48 +0200

    Add handling protected properties and methods to OTGS_TestCase
    https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlga-568

--- phpunit/otgs-testcase.php
+++ phpunit/otgs-testcase.php
@@ -169,4 +169,63 @@ abstract class OTGS_TestCase extends PHPUnit_Framework_TestCase {
 		$responder = \WP_Mock::onHookAdded( $action, $type )->with( $callback, $priority, $args );
 		$responder->perform( array( $intercept, 'intercepted' ) );
 	}
+
+	/**
+	 * Get an object protected property.
+	 *
+	 * @param object $object        Object.
+	 * @param string $property_name Property name.
+	 *
+	 * @return mixed
+	 *
+	 * @throws ReflectionException Reflection exception.
+	 */
+	private function get_property( $object, $property_name ) {
+		$reflection_class = new \ReflectionClass( $object );
+
+		$property = $reflection_class->getProperty( $property_name );
+		$property->setAccessible( true );
+		$value = $property->getValue( $object );
+		$property->setAccessible( false );
+
+		return $value;
+	}
+
+	/**
+	 * Set an object protected property.
+	 *
+	 * @param object $object        Object.
+	 * @param string $property_name Property name.
+	 * @param mixed  $value         Property vale.
+	 *
+	 * @throws ReflectionException Reflection exception.
+	 */
+	private function set_property( $object, $property_name, $value ) {
+		$reflection_class = new \ReflectionClass( $object );
+
+		$property = $reflection_class->getProperty( $property_name );
+		$property->setAccessible( true );
+		$property->setValue( $object, $value );
+		$property->setAccessible( false );
+	}
+
+	/**
+	 * Set an object protected method accessibility.
+	 *
+	 * @param object $object      Object.
+	 * @param string $method_name Property name.
+	 * @param bool   $accessible  Property vale.
+	 *
+	 * @return ReflectionMethod
+	 *
+	 * @throws ReflectionException Reflection exception.
+	 */
+	private function set_method_accessibility( $object, $method_name, $accessible = 'true' ) {
+		$reflection_class = new \ReflectionClass( $object );
+
+		$method = $reflection_class->getMethod( $method_name );
+		$method->setAccessible( $accessible );
+
+		return $method;
+	}
 }
